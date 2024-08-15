3261956a7aa20364b2635e6422338c3ee919e7c0
mstsurnalyakter <surnalyms@gmail.com>
2024-08-15 10:12:17 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -2,12 +2,54 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Shop Ease");
-})
+//building middleware
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-app.listen(port,()=>{
-    console.log(`ShopEase is running at: http://localhost:${port}`);
-})
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("shop-ease");
+
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from LearnEnglish Server..");
+});
+
+app.listen(port, () => {
+  console.log(`ShopEase is running at : http://localhost:${port}`);
+});
