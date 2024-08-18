b792a0e541a5307c1dd2549af4cc99d1fbad3113
mstsurnalyakter <surnalyms@gmail.com>
2024-08-18 11:50:22 +0600

    update: index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://shop-ease-4a820.web.app",
   ],
   credentials: true,
   optionSuccessStatus: 200,
